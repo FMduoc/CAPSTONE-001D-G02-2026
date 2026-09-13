c648460bf32e512b70c0af72a9f7c7171b04d4af
estebanquito123 <estebantoro832@gmail.com>
2026-09-13 02:40:17 -0300

    merge

---
+++ Fase 2/Evidencias de proyecto/Evidencia de sistema o aplicación/SGCP/src/main.ts
@@ -0,0 +1,17 @@
+/*
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
+*/
+
+//main.ts
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'; 
+import { AppModule } from './app/app.module';
+
+// Ensure the AppModule is correctly bootstrapped
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch(err => console.error(err));
